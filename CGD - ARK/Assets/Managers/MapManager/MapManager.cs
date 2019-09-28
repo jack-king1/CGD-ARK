9682060c94243ed1de85dec80dd5a5eea6f80841
King <jack3.king@live.uwe.ac.uk>
2019-09-28 20:29:50 +0100

    tweaks

--- CGD - ARK/Assets/Managers/MapManager/MapManager.cs
+++ CGD - ARK/Assets/Managers/MapManager/MapManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class MapManager : MonoBehaviour
 {
-    [SerializeField] private int col;
-    [SerializeField] private int row;
+    [SerializeField]private bool m_randomTiles;
+
+    [SerializeField] private int m_numberOfColumns;
+    [SerializeField] private int m_numberOfRows;
+
     private Vector2[][] tile_Positions;
-    private GameObject[] tiles;
+    private GameObject[] tile_types;
 
     private int col_height = 0;
     private int row_width = 0;
@@ -16,18 +19,39 @@ public class MapManager : MonoBehaviour
     private void Awake()
     {
         //Initialise Tile Positions.
-        for (int i = 0; i < col; ++i)
+        for (int i = 0; i < m_numberOfColumns; ++i)
         {
-            for (int j = 0; j < row; ++j)
+            for (int j = 0; j < m_numberOfRows; ++j)
             {
-                //Set New Tile Position
+                //Local Position For Readability
+                Vector2 pos;
 
-                //Set the X Positon - This ensures the middle is always 0,0.
-                tile_Positions[i][j].x = row_width * i - (i * (col /2));
+                //Set New Tile Position
+                //Set the X Positon
+                tile_Positions[i][j].x = row_width * i - (i * (m_numberOfColumns /2));
+                pos.x = tile_Positions[i][j].x;
 
                 //Set the Y Position
-                tile_Positions[i][j].y = col_height * j;
+                tile_Positions[i][j].y = col_height * j - ( j * (m_numberOfRows / 2));
+                pos.y = tile_Positions[i][j].y;
+
+                Instantiate(tile_types[tile_selector()], new Vector2(pos.x, pos.y), Quaternion.identity);
             }
+        }
+
+        //After tiles have been instantiated call the camera setup function which
+        // Places camera positions
+    }
+
+    int tile_selector()
+    {
+        if(!m_randomTiles)
+        {
+            return 0;
+        }
+        else
+        {
+            return Random.Range(0, tile_types.Length -1);
         }
     }
 }
