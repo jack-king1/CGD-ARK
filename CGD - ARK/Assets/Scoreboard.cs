b56baba5119920d526eb8ceef914dd5a31536b2b
Jack <Jack2.Lynham@live.uwe.ac.uk>
2019-10-01 16:53:11 +0100

    Wip Scoreboard

---
+++ CGD - ARK/Assets/Scoreboard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+
+{   Dictionary<string, int> playerScores;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerScores = new Dictionary<string, int>();
+    }
+
+    void Init()
+    {
+        if(playerScores !=null )
+        {
+            return;
+        }
+    }
+    public int GetScore(string username,   )
+    {
+        Init();
+        if(playerScores.ContainsKey(username) == false)
+        {
+            return 0;
+        }
+   
+        return playerScores [username];
+      
+    }
+
+    public void SetScore (string username, int value)
+    {
+        Init();
+        if(playerScores.ContainsKey (username) == false)
+        {
+            playerScores[username] = new Dictionary<string, int>();
+        }
+        playerScores[username] = value;
+    }
+
+    public void ChangeScore(string username, int value)
+    {
+        Init();
+    }
+}
