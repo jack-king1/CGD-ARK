a8822a0dbc3dcd37cca6bb1f305fa472b77055ab
King <jack3.king@live.uwe.ac.uk>
2019-10-01 13:40:30 +0100

    Merged with lynny - fixed missing scripts

--- CGD - ARK/Assets/Scripts/Enemy/Enemy.cs
+++ CGD - ARK/Assets/Scripts/Enemy/Enemy.cs
@@ -24,6 +24,7 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        chasing = false;
         currentWaypoint = waypoints[StartingWaypoint()];
     }
 
