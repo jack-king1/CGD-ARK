0ffe3d8a95b5da55044dc6b335c2d94880739939
King <jack3.king@live.uwe.ac.uk>
2019-10-07 00:02:41 +0100

    Working on dino spawner with score/time played
    WIP

--- CGD - ARK/Assets/Scripts/Enemy/DinoSpawner.cs
+++ CGD - ARK/Assets/Scripts/Enemy/DinoSpawner.cs
@@ -7,14 +7,47 @@ public class DinoSpawner : MonoBehaviour
     //Spawn enemies around player.
     //The more score the player has the closer the enemies spawn.
     //The longer the game has gone on the more enemies spawn.
+    [SerializeField] private GameObject dinoPrefab;
+    [SerializeField] private float newDinoTimer = 0.0f;
+    [SerializeField] private int initialDinoAmount;
+    [SerializeField] private float spawnRadiusMin;
+    [SerializeField] private float spawnRadiusMax;
+    private Transform playerTransform;
 
     bool initialSpawn = false;
+    
     private void Start()
     {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+
         if(!initialSpawn)
         {
-
+            for(int i = 0; i < initialDinoAmount; ++i)
+            {
+                Vector3 spawnPos = RandomCircle(playerTransform.position, RandomRadius());
+                Instantiate(dinoPrefab, spawnPos, gameObject.transform.rotation);
+            }
         }
     }
 
+    private void Update()
+    {
+        //Spawn Dinos based on score and time in game.
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius)
+    {
+        float ang = Random.value * 360;
+        Vector3 pos;
+
+        pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(ang * Mathf.Deg2Rad);
+        pos.z = dinoPrefab.transform.position.z;
+        return pos;
+    }
+
+    float RandomRadius()
+    {
+        return Random.Range(spawnRadiusMin, spawnRadiusMax);
+    }
 }
---
+++ CGD - ARK/Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] private int currentScore;
+
+    public void addScore(int score)
+    {
+        currentScore += score;
+    }
+
+    public int getScore()
+    {
+        return currentScore;
+    }
+
+    public void ResetScore()
+    {
+        currentScore = 0;
+    }
+}
